8ebab22b7437f126df5beeb04b7d924bb47787ad
raghab singh <raghabsingh99@gmail.com>
2024-08-21 20:50:46 -0500

    load data into dataframe

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-from fpdf import FPDF
 import pandas as pd
 
-pdf = FPDF(orientation="P",unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False,margin=0)
+import glob
 
-df= pd.read_csv("topics.csv")
+filepaths = glob.glob("invoices/*.xlsx")
 
-for index,row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times",style="B",size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
-
-    pdf.line(10, 21, 200, 21)
-
-    pdf.ln(260)
-    pdf.set_font(family="Times", style="I", size=8)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-    for i in range(row["Pages"]-1):
-        pdf.add_page()
-
-        pdf.ln(260)
-        pdf.set_font(family="Times", style="I", size=8)
-        pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-
-
-
-pdf.output("output.pdf")
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
